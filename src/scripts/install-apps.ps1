46baa2c250fb704f587212a1e4b1ea352ca204d8
Thomas Carter <thomas@techythomas.com>
2024-09-10 23:29:59 +0100

    Refactored, params
    - Split into sub files. Added params to run just config or app install. No params run all scripts.

---
+++ src/scripts/install-apps.ps1
@@ -0,0 +1,19 @@
+# Install packages
+
+Write-Host "Installing packages..." -ForegroundColor Cyan
+
+InstallPackage -PackageId "Valve.Steam" -PackageName "Steam"
+InstallPackage -PackageId "Discord.Discord" -PackageName "Discord"
+InstallPackage -PackageId "Parsec.Parsec" -PackageName "Parsec"
+InstallPackage -PackageId "Anysphere.Cursor" -PackageName "Cursor IDE"
+InstallPackage -PackageId "Microsoft.VisualStudioCode" -PackageName "VS Code"
+InstallPackage -PackageId "Git.Git" -PackageName "Git"
+InstallPackage -PackageId "Axosoft.GitKraken" -PackageName "GitKraken"
+InstallPackage -PackageId "Obsidian.Obsidian" -PackageName "Obsidian"
+InstallPackage -PackageId "Microsoft.PowerToys" -PackageName "PowerToys"
+InstallPackage -PackageId "M2Team.NanaZip" -PackageName "NanaZip"
+InstallPackage -PackageId "AgileBits.1Password" -PackageName "1Password"
+InstallPackage -PackageId "Ytmdesktop.Ytmdesktop" -PackageName "YouTube Music"
+InstallPackage -PackageId "9WZDNCRD29V9" -PackageName "Offce 365"
+
+Write-Host "Apps installed" -ForegroundColor Green
--- src/scripts/set-config.ps1
+++ src/scripts/set-config.ps1
@@ -1,32 +1,3 @@
-# Includes
-
-. ".\functions.ps1"
-
-# Intro
-
-$title = @"
-
-  _______        _        _______ _
- |__   __|      | |      |__   __| |
-    | | ___  ___| |__  _   _| |  | |__   ___  _ __ ___   __ _ ___
-    | |/ _ \/ __| '_ \| | | | |  | '_ \ / _ \| '_ ` _ \ / _` / __|
-    | |  __/ (__| | | | |_| | |  | | | | (_) | | | | | | (_| \__ \
-    |_|\___|\___|_| |_|\__, |_|  |_| |_|\___/|_| |_| |_|\__,_|___/
-                        __/ |
-                       |___/
-
- __          ___          _____      _                  _____           _       _
- \ \        / (_)        / ____|    | |                / ____|         (_)     | |
-  \ \  /\  / / _ _ __   | (___   ___| |_ _   _ _ __   | (___   ___ _ __ _ _ __ | |_
-   \ \/  \/ / | | '_ \   \___ \ / _ \ __| | | | '_ \   \___ \ / __| '__| | '_ \| __|
-    \  /\  /  | | | | |  ____) |  __/ |_| |_| | |_) |  ____) | (__| |  | | |_) | |_
-     \/  \/   |_|_| |_| |_____/ \___|\__|\__,_| .__/  |_____/ \___|_|  |_| .__/ \__|
-                                              | |                        | |
-                                              |_|                        |_|
-"@
-
-Write-Host $title -ForegroundColor Magenta
-
 # Registry tweaks
 
 Write-Host "Making reqistry tweaks..." -ForegroundColor Cyan
@@ -50,7 +21,7 @@ UpdateRegistry -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Per
 UpdateRegistry -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "EnableTransparency" -Value 1 # Enable transparent effect
 UpdateRegistry -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "ColorPrevalence" -Value 0 # Disable accent colour on taskbar and windows
 
-$userSID = (Get-WmiObject win32_useraccount | ? {$_.name -eq $Env:USERNAME}).SID
+$userSID = (Get-WmiObject win32_useraccount | ? { $_.name -eq $Env:USERNAME }).SID
 UpdateRegistry -Path "Registry::\HKEY_USERS\${userSID}\Control Panel\International" -Name "sShortDate" -Value "dd MMM yyyy" -Type String # Set short date format e.g. 04 Sep 2024
 
 # Restart Windows Explorer to apply registry changes
@@ -59,20 +30,4 @@ Write-Host "Restarting Windows Explorer..." -ForegroundColor Cyan
 taskkill /f /im explorer.exe
 Start-Process explorer.exe
 
-# Install packages
-
-Write-Host "Installing packages..." -ForegroundColor Cyan
-
-InstallPackage -PackageId "Valve.Steam" -PackageName "Steam"
-InstallPackage -PackageId "Discord.Discord" -PackageName "Discord"
-InstallPackage -PackageId "Parsec.Parsec" -PackageName "Parsec"
-InstallPackage -PackageId "Anysphere.Cursor" -PackageName "Cursor IDE"
-InstallPackage -PackageId "Microsoft.VisualStudioCode" -PackageName "VS Code"
-InstallPackage -PackageId "Git.Git" -PackageName "Git"
-InstallPackage -PackageId "Axosoft.GitKraken" -PackageName "GitKraken"
-InstallPackage -PackageId "Obsidian.Obsidian" -PackageName "Obsidian"
-InstallPackage -PackageId "Microsoft.PowerToys" -PackageName "PowerToys"
-InstallPackage -PackageId "M2Team.NanaZip" -PackageName "NanaZip"
-InstallPackage -PackageId "AgileBits.1Password" -PackageName "1Password"
-InstallPackage -PackageId "Ytmdesktop.Ytmdesktop" -PackageName "YouTube Music"
-InstallPackage -PackageId "9WZDNCRD29V9" -PackageName "Offce 365"
+Write-Host "Windows Config Applied" -ForegroundColor Green
