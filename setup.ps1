e74e1fb0e748cf7fdd3f522f9d7276361d6435a9
Thomas Carter <thomas@techythomas.com>
2024-09-10 01:23:38 +0100

    Added more apps

--- setup.ps1
+++ setup.ps1
@@ -63,6 +63,12 @@ Start-Process explorer.exe
 
 Write-Host "Installing packages..." -ForegroundColor Cyan
 
+InstallPackage -PackageId "Valve.Steam" -PackageName "Steam"
 InstallPackage -PackageId "Discord.Discord" -PackageName "Discord"
+InstallPackage -PackageId "Parsec.Parsec" -PackageName "Parsec"
 InstallPackage -PackageId "Microsoft.VisualStudioCode" -PackageName "VS Code"
+InstallPackage -PackageId "Git.Git" -PackageName "Git"
 InstallPackage -PackageId "Microsoft.PowerToys" -PackageName "PowerToys"
+InstallPackage -PackageId "AgileBits.1Password" -PackageName "1Password"
+InstallPackage -PackageId "Ytmdesktop.Ytmdesktop" -PackageName "YouTube Music"
+InstallPackage -PackageId "9WZDNCRD29V9" -PackageName "Offce 365"
