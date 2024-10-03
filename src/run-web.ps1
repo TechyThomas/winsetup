2525135b2d3e30e89fa418efaac0d1ddd889ed7f
Thomas Carter <thomas@techythomas.com>
2024-10-03 23:04:53 +0100

    Create run file for web command

---
+++ src/run-web.ps1
@@ -0,0 +1,52 @@
+param (
+  [switch]$config,
+  [switch]$apps
+)
+
+$baseUrl = "https://raw.githubusercontent.com/TechyThomas/winsetup/main"
+
+# Function to download and execute scripts
+function Include-Script {
+    param (
+        [string]$ScriptPath
+    )
+    $scriptUrl = "$baseUrl/$ScriptPath"
+    $scriptContent = (Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing).Content
+    Invoke-Expression $scriptContent
+}
+
+# Include functions
+Include-Script "includes/functions.ps1"
+
+# Intro
+
+$title = @"
+
+  _______        _        _______ _
+ |__   __|      | |      |__   __| |
+    | | ___  ___| |__  _   _| |  | |__   ___  _ __ ___   __ _ ___
+    | |/ _ \/ __| '_ \| | | | |  | '_ \ / _ \| '_ ` _ \ / _` / __|
+    | |  __/ (__| | | | |_| | |  | | | | (_) | | | | | | (_| \__ \
+    |_|\___|\___|_| |_|\__, |_|  |_| |_|\___/|_| |_| |_|\__,_|___/
+                        __/ |
+                       |___/
+
+ __          ___          _____      _                  _____           _       _
+ \ \        / (_)        / ____|    | |                / ____|         (_)     | |
+  \ \  /\  / / _ _ __   | (___   ___| |_ _   _ _ __   | (___   ___ _ __ _ _ __ | |_
+   \ \/  \/ / | | '_ \   \___ \ / _ \ __| | | | '_ \   \___ \ / __| '__| | '_ \| __|
+    \  /\  /  | | | | |  ____) |  __/ |_| |_| | |_) |  ____) | (__| |  | | |_) | |_
+     \/  \/   |_|_| |_| |_____/ \___|\__|\__,_| .__/  |_____/ \___|_|  |_| .__/ \__|
+                                              | |                        | |
+                                              |_|                        |_|
+"@
+
+Write-Host $title -ForegroundColor Magenta
+
+if ($config -or (-not $config -and -not $apps)) {
+    Include-Script "scripts/set-config.ps1"
+}
+
+if ($apps -or (-not $config -and -not $apps)) {
+    Include-Script "scripts/install-apps.ps1"
+}
